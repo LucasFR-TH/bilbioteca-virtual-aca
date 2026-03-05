5091662d6ff17a0e21ce63ee9a7e3ad851f1dd04
LucasFRTH <lucas.f.roldan@gamil.com>
2026-03-05 10:32:18 -0300

    creacion del MVC para controlar el desarrollo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace bilbioteca_virtual_aca.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
